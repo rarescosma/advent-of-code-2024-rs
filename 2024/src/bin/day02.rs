16a85db845c84e9c5aeedafcd4a1ba7ebb210ee8
Rareș Cosma <rares@getbetter.ro>
2024-12-03 08:56:52 +0200

    02: fmt

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -2,20 +2,23 @@ fn solve() -> (usize, usize) {
     let input = include_str!("../../inputs/02.in")
         .lines()
         .map(|line| {
-            line.split_whitespace().map(|el| el.parse().unwrap()).collect::<Vec<_>>()
+            line.split_whitespace()
+                .map(|el| el.parse().unwrap())
+                .collect::<Vec<_>>()
         })
         .collect::<Vec<Vec<i32>>>();
 
     let p1 = input.clone().into_iter().filter(is_valid).count();
 
-    let p2 = input.into_iter().filter(|row| {
-        is_valid(row) || permute(row).any(|del_row| is_valid(&del_row))
-    }).count();
+    let p2 = input
+        .into_iter()
+        .filter(|row| is_valid(row) || permute(row).any(|del_row| is_valid(&del_row)))
+        .count();
 
     (p1, p2)
 }
 
-fn permute(row: &[i32]) -> impl Iterator<Item=Vec<i32>> + '_ {
+fn permute(row: &[i32]) -> impl Iterator<Item = Vec<i32>> + '_ {
     (0..row.len()).map(|idx| {
         let mut cp = row.to_owned();
         cp.remove(idx);
@@ -24,7 +27,6 @@ fn permute(row: &[i32]) -> impl Iterator<Item=Vec<i32>> + '_ {
 }
 
 fn is_valid(row: &Vec<i32>) -> bool {
-
     let rev = row.clone().into_iter().rev().collect::<Vec<_>>();
 
     let mut sorted = row.clone();
