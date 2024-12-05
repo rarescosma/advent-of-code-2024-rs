f6672854a58fba377f58986670c5ec9ad7071c74
Rareș Cosma <rares@getbetter.ro>
2024-12-05 08:57:08 +0200

    05: optimize
    - get rid of allocations
    - use a const array instead of HashMap<usize, HashSet<usize>>

--- 2024/src/bin/day05.rs
+++ 2024/src/bin/day05.rs
@@ -1,72 +1,56 @@
-use aoc_prelude::*;
 use std::cmp::Ordering;
+
+type MapType = [[bool; 100]; 100];
+
+fn make_map<const N: usize>() -> [[bool; N]; N] {
+    [[false; N]; N]
+}
 
 fn solve() -> (usize, usize) {
     let (ord, rep) = include_str!("../../inputs/05.in")
         .split_once("\n\n")
         .unwrap();
 
-    let mut before_set = HashMap::new();
+    let mut before_set = make_map::<100>();
 
     ord.lines().for_each(|line| {
         let (left, right) = line.split_once('|').unwrap();
         let r_num = right.parse::<usize>().unwrap();
         let l_num = left.parse::<usize>().unwrap();
-        before_set
-            .entry(l_num)
-            .or_insert(HashSet::new())
-            .insert(r_num);
+        before_set[l_num][r_num] = true
     });
 
-    let reports = rep
-        .lines()
-        .map(|line| {
-            line.split(',')
-                .filter_map(|n| n.parse::<usize>().ok())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+    let mut p1 = 0;
+    let mut p2 = 0;
 
-    let p1 = reports
-        .iter()
-        .filter_map(|rep| {
-            if report_ok(rep, &before_set) {
-                Some(rep[rep.len() / 2])
-            } else {
-                None
-            }
-        })
-        .sum();
+    let mut rep_buf = Vec::with_capacity(100);
 
-    let p2 = reports
-        .into_iter()
-        .filter(|rep| !report_ok(rep, &before_set))
-        .map(|mut rep| {
-            rep.sort_by(|left, right| {
-                if ord_ok((left, right), &before_set) {
+    rep.lines().for_each(|line| {
+        rep_buf.clear();
+        rep_buf.extend(line.split(',').filter_map(|n| n.parse::<usize>().ok()));
+
+        if report_ok(&rep_buf, &before_set) {
+            p1 += rep_buf[rep_buf.len() / 2]
+        } else {
+            rep_buf.sort_by(|&left, &right| {
+                if before_set[left][right] {
                     Ordering::Greater
                 } else {
                     Ordering::Less
                 }
             });
-            rep[rep.len() / 2]
-        })
-        .sum();
+            p2 += rep_buf[rep_buf.len() / 2]
+        }
+    });
 
     (p1, p2)
 }
 
-fn ord_ok((left, right): (&usize, &usize), before_set: &HashMap<usize, HashSet<usize>>) -> bool {
-    if let Some(val) = before_set.get(right) {
-        !val.contains(left)
-    } else {
-        false
-    }
-}
-
-fn report_ok(report: &[usize], before_set: &HashMap<usize, HashSet<usize>>) -> bool {
-    let _ord_ok = |(left, right)| ord_ok((left, right), before_set);
-    report.iter().zip(report.iter().skip(1)).all(_ord_ok)
+fn report_ok(report: &[usize], before_set: &MapType) -> bool {
+    report
+        .iter()
+        .zip(report.iter().skip(1))
+        .all(|(&left, &right)| before_set[left][right])
 }
 
 aoc_2024::main! {
