39e6a878be937667ba44aadd3b3804d0f44948df
Rareș Cosma <rares@getbetter.ro>
2024-12-08 09:24:29 +0200

    08: use boolean arrays instead of sets

--- 2024/src/bin/day08.rs
+++ 2024/src/bin/day08.rs
@@ -1,6 +1,5 @@
 use aoc_2dmap::prelude::*;
-use aoc_prelude::{BTreeMap, HashSet, Itertools};
-use std::iter::once;
+use aoc_prelude::{BTreeMap, Itertools};
 
 fn solve() -> (usize, usize) {
     let input = include_str!("../../inputs/08.in").lines().collect_vec();
@@ -14,50 +13,45 @@ fn solve() -> (usize, usize) {
         }
     }
 
-    let mut p1_nodes = HashSet::new();
-    let mut p2_nodes = HashSet::<Pos>::new();
-    let mut buf = Vec::with_capacity(256);
+    let mut p1_vec = vec![false; (map.size.x * map.size.y) as usize];
+    let mut p2_vec = vec![false; (map.size.x * map.size.y) as usize];
 
     for antennas in h_map.values() {
         antennas.iter().tuple_combinations().for_each(|(p1, p2)| {
-            p1_nodes.extend(antinodes(p1, p2, &map.size));
-            antinodes_p2(p1, p2, &map.size, &mut buf);
-            p2_nodes.extend(buf.iter());
+            antinodes(p1, p2, &map.size, &mut p1_vec, &mut p2_vec);
+            antinodes(p2, p1, &map.size, &mut p1_vec, &mut p2_vec);
         });
     }
 
-    (p1_nodes.len(), p2_nodes.len())
+    let p1 = p1_vec.into_iter().filter(|&x| x).count();
+    let p2 = p2_vec.into_iter().filter(|&x| x).count();
+
+    (p1, p2)
+}
+
+fn antinodes(a1: &Pos, a2: &Pos, map_size: &MapSize, p1_buf: &mut [bool], p2_buf: &mut [bool]) {
+    let dist = *a2 - *a1;
+
+    let mut cand = *a2;
+    let mut i = 0;
+
+    while within(&cand, map_size) {
+        let idx = index(&cand, map_size);
+        p2_buf[idx] = true;
+        if i == 1 {
+            p1_buf[idx] = true;
+        }
+        cand += dist;
+        i += 1;
+    }
 }
 
 fn within(p: &Pos, map_size: &MapSize) -> bool {
     p.x >= 0 && p.x < map_size.x && p.y >= 0 && p.y < map_size.y
 }
 
-fn antinodes<'a>(a1: &Pos, a2: &Pos, map_size: &'a MapSize) -> impl Iterator<Item = Pos> + 'a {
-    let dist = *a2 - *a1;
-    let cand1 = *a2 + dist;
-    let cand2 = *a1 + (-dist.x, -dist.y).into();
-    once(cand1)
-        .chain(once(cand2))
-        .filter(|p| within(p, map_size))
-}
-
-fn antinodes_p2(a1: &Pos, a2: &Pos, map_size: &MapSize, buf: &mut Vec<Pos>) {
-    buf.clear();
-    let dist = *a2 - *a1;
-    let a_dist: Pos = (-dist.x, -dist.y).into();
-
-    let mut cand = *a2;
-    while within(&cand, map_size) {
-        buf.push(cand);
-        cand += dist;
-    }
-
-    cand = *a1;
-    while within(&cand, map_size) {
-        buf.push(cand);
-        cand += a_dist;
-    }
+fn index(p: &Pos, map_size: &MapSize) -> usize {
+    (p.y * map_size.x + p.x) as usize
 }
 
 aoc_2024::main! {
