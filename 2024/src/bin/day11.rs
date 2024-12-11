3fbbdd52a10a59339e229f4623b6da2fd33ecfc8
Rareș Cosma <rares@getbetter.ro>
2024-12-11 13:02:48 +0200

    11: optimize
    - get rid of enum
    - look up table for powers of ten
    - use ArrayVec instead of Vec for changelog
    
    went down from 6ms to 4ms (much wow!)

--- 2024/src/bin/day11.rs
+++ 2024/src/bin/day11.rs
@@ -4,10 +4,13 @@
 //! apply them sequentially.
 
 use aoc_prelude::num_integer::Integer;
-use aoc_prelude::HashMap;
+use aoc_prelude::{ArrayVec, HashMap};
 
-type Int = u64;
-const TEN: Int = 10u64;
+type Int = i64;
+// const TEN: Int = 10i64;
+const TENS: [Int; 10] = [
+    1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
+];
 
 fn solve() -> (Int, Int) {
     let mut tally: HashMap<Int, Int> = include_str!("../../inputs/11.in")
@@ -16,7 +19,7 @@ fn solve() -> (Int, Int) {
         .filter_map(|el| el.parse::<Int>().ok().map(|key| (key, 1)))
         .collect();
 
-    let mut changes = Vec::with_capacity(16384);
+    let mut changes = ArrayVec::<(Int, Int), 16384>::new();
 
     for _ in 0..25 {
         epoch(&mut tally, &mut changes);
@@ -31,24 +34,6 @@ fn solve() -> (Int, Int) {
     (p1, p2)
 }
 
-enum Change {
-    Decr(Int, Int),
-    Incr(Int, Int),
-}
-
-impl Change {
-    fn apply(&self, tally: &mut HashMap<Int, Int>) {
-        match self {
-            Change::Decr(k, delta) => {
-                *tally.get_mut(k).unwrap() -= delta;
-            }
-            Change::Incr(k, delta) => {
-                *tally.entry(*k).or_insert(0) += delta;
-            }
-        }
-    }
-}
-
 fn num_digits(num: Int) -> u32 {
     num.checked_ilog10().unwrap_or(0) + 1
 }
@@ -56,32 +41,32 @@ fn num_digits(num: Int) -> u32 {
 fn split(num: Int) -> Option<(Int, Int)> {
     let digits = num_digits(num);
     let (q, r) = digits.div_rem(&2);
-    (r == 0).then(|| num.div_rem(&TEN.pow(q)))
+    (r == 0).then(|| num.div_rem(&TENS[q as usize]))
 }
 
-fn epoch(tally: &mut HashMap<Int, Int>, changes: &mut Vec<Change>) {
+fn epoch(tally: &mut HashMap<Int, Int>, changes: &mut ArrayVec<(Int, Int), 16384>) {
     changes.clear();
 
     // all zeroes become ones
     if let Some(num_zeroes) = tally.remove(&0) {
-        changes.push(Change::Incr(1, num_zeroes));
+        changes.push((1, num_zeroes));
     }
 
     for (&key, &val) in tally.iter() {
         if val > 0 {
-            changes.push(Change::Decr(key, val));
+            changes.push((key, -val));
 
             if let Some((left, right)) = split(key) {
-                changes.push(Change::Incr(left, val));
-                changes.push(Change::Incr(right, val));
+                changes.push((left, val));
+                changes.push((right, val));
             } else {
-                changes.push(Change::Incr(key * 2024, val));
+                changes.push((key * 2024, val));
             }
         }
     }
 
-    for change in changes {
-        change.apply(tally);
+    for (k, delta) in changes {
+        *tally.entry(*k).or_insert(0) += *delta;
     }
 }
 
