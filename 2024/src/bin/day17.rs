1bda08346cd3ea00a4638306b3ae31a05108bde0
Rareș Cosma <rares@getbetter.ro>
2024-12-17 15:30:52 +0200

    17: ascii art docs

--- 2024/src/bin/day17.rs
+++ 2024/src/bin/day17.rs
@@ -2,12 +2,20 @@
 //!
 //! Part 1: code-monkey.
 //!
-//! Part 2: the "A-ha" moment was realising a digit is only determined
-//! by 3 bits of the "a" number and no matter how far left those digits get
+//! Part 2: the "A-ha" moment was realising an output digit is only determined
+//! by 3 bits of the "A" registry and no matter how far left those digits get
 //! shifted, the output digit stays the same.
 //!
 //! At each step we produce candidates that output the required digit,
-//! then use these as the new "initial" values for the next iteration.
+//! then use these as the new "initial" values for the next iteration, in
+//! which they'll get shifted 3 positions to the left.
+//!
+//! So, if A = 0b111_011_100_101
+//!             |___|___|
+//!               ^   ^
+//!               |   |
+//! last digit ---+   |
+//! next to last ---+-+
 
 use std::mem;
 
