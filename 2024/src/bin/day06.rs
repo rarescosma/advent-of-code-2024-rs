3d1132078c3f78f0028a66e34e483547fb5afab3
Rareș Cosma <rares@getbetter.ro>
2024-12-26 18:11:44 +0200

    06: gallivant faster

--- 2024/src/bin/day06.rs
+++ 2024/src/bin/day06.rs
@@ -8,26 +8,16 @@
 //!
 //! Gotcha for part 2: when using a Vec as boolean array, you gotta `.fill(false)` instead of
 //! `.clear()` it.
+//!
+//! Optimized to use teleport maps.
+
+use std::thread::available_parallelism;
+
 use aoc_2dmap::prelude::*;
-use aoc_prelude::num_integer::Integer;
+use aoc_prelude::{num_integer::Integer, Itertools};
+use rayon::prelude::*;
 
-struct Buffers {
-    states: Vec<bool>,
-    visited_pos: Vec<bool>,
-}
-
-impl Buffers {
-    fn allocate(size: MapSize) -> Self {
-        let states = vec![false; (size.x * size.y) as usize * 4];
-        let visited_pos = vec![false; (size.x * size.y) as usize];
-        Self { states, visited_pos }
-    }
-
-    fn clear(&mut self) {
-        self.states.fill(false);
-        self.visited_pos.fill(false);
-    }
-}
+type Teleport = Vec<Pos>;
 
 fn solve() -> (usize, usize) {
     let input = include_str!("../../inputs/06.in");
@@ -35,62 +25,126 @@ fn solve() -> (usize, usize) {
         input.bytes().position(|x| x == b'\n').unwrap(),
         input.bytes().filter(|x| *x == b'\n').count(),
     ));
-    let mut map = Map::new(map_size, input.bytes().filter(|&c| c != b'\n'));
 
+    let map = Map::new(map_size, input.bytes().filter(|&c| c != b'\n'));
     let start = find_tile(&map, b'^');
+    let teleport = make_teleport(&map);
 
-    let mut buffers = Buffers::allocate(map.size);
-    has_cycle(&map, start, &mut buffers);
-    let p1 = buffers.visited_pos.iter().filter(|&x| *x).count();
+    let mut pos = start;
+    let mut dir = 0;
 
-    let mut p2 = 0;
-    for hash in buffers.visited_pos.clone().iter().enumerate().filter(|x| *x.1).map(|x| x.0) {
-        let (x, y) = hash.div_rem(&(map.size.y as usize));
+    let mut visited = vec![false; (map.size.x * map.size.y) as usize];
+    while map.within(pos + ORTHOGONAL[dir]) {
+        let new_pos = teleport[key(pos, dir, map.size)];
 
-        let cand = Pos::from((x, y));
-        if map.get(cand) == Some(b'.') {
-            map[cand] = b'#';
-            if has_cycle(&map, start, &mut buffers) {
-                p2 += 1;
+        for x in pos.x.min(new_pos.x)..=new_pos.x.max(pos.x) {
+            for y in pos.y.min(new_pos.y)..=new_pos.y.max(pos.y) {
+                if map.within(Pos::new(x, y)) {
+                    visited[(x * map.size.y + y) as usize] = true;
+                }
             }
-            map[cand] = b'b';
         }
+
+        pos = new_pos;
+        dir = turn_right(dir);
     }
+
+    let p1 = visited.iter().filter(|&x| *x).count();
+
+    let obstacles = visited
+        .iter()
+        .enumerate()
+        .filter(|x| *x.1)
+        .map(|(hash, _)| {
+            let (x, y) = hash.div_rem(&(map.size.y as usize));
+            Pos::from((x, y))
+        })
+        .collect_vec();
+
+    let p2 = obstacles
+        .chunks(obstacles.len() / available_parallelism().unwrap().get())
+        .par_bridge()
+        .map(|chunk| {
+            let mut seen = vec![false; (map.size.x * map.size.y) as usize * 4];
+            chunk
+                .iter()
+                .map(|obs| has_cycle(&map, start, *obs, &teleport, &mut seen) as usize)
+                .sum::<usize>()
+        })
+        .sum();
 
     (p1, p2)
 }
 
-fn has_cycle(map: &Map<u8>, start: Pos, buffers: &mut Buffers) -> bool {
-    let mut cur = start;
-    let mut dir = 0;
-    buffers.clear();
-    buffers.visited_pos[(start.x * map.size.y + start.y) as usize] = true;
-
-    loop {
-        cur += ORTHOGONAL[dir];
-        if cur.x < 0 || cur.y < 0 || cur.x == map.size.x || cur.y == map.size.y {
-            return false;
-        }
-        let c = map[cur];
-        let hash = ((cur.x * map.size.y + cur.y) * 4) as usize + dir;
-        if c == b'#' {
-            // backtrack + turn right
-            let opposite = ORTHOGONAL[turn_back(dir)];
-            cur += opposite;
-            dir = turn_right(dir);
-        } else {
-            // cycle detected
-            if buffers.states[hash] {
-                return true;
+fn make_teleport(map: &Map<u8>) -> Teleport {
+    let mut teleport = vec![Pos::new(0, 0); (map.size.x * map.size.y) as usize * 4];
+    // Initially, all points teleport outside the map
+    for x in 0..map.size.x {
+        for y in 0..map.size.y {
+            for dir in 0..4 {
+                let out = match ORTHOGONAL[dir] {
+                    NORTH => Pos::new(x, -1),
+                    WEST => Pos::new(-1, y),
+                    SOUTH => Pos::new(x, map.size.y),
+                    EAST => Pos::new(map.size.x, y),
+                    _ => unreachable!(),
+                };
+                teleport[key(Pos::new(x, y), dir, map.size)] = out;
             }
         }
-        buffers.states[hash] = true;
-        buffers.visited_pos[(cur.x * map.size.y + cur.y) as usize] = true;
     }
+
+    // Every obstacle acts as a "black hole", pulling guards towards it
+    for (obs, _) in map.iter().map(|p| (p, map[p])).filter(|(_, c)| *c == b'#') {
+        for dir in 0..4 {
+            // We're moving *away* from the obstacle, so the teleport key is for the opposite
+            let mut cur = obs + ORTHOGONAL[dir];
+            while map.within(cur) && map[cur] != b'#' {
+                teleport[key(cur, turn_back(dir), map.size)] = obs + ORTHOGONAL[dir];
+                cur += ORTHOGONAL[dir];
+            }
+        }
+    }
+    teleport
 }
 
-#[inline(always)]
+fn has_cycle(map: &Map<u8>, start: Pos, obs: Pos, teleport: &Teleport, seen: &mut [bool]) -> bool {
+    seen.fill(false);
+    let mut cur = start;
+    let mut dir = 0;
+
+    while map.within(cur) {
+        let k_ = key(cur, dir, map.size);
+        if seen[k_] {
+            return true;
+        } else {
+            seen[k_] = true;
+        }
+
+        let mut next = teleport[k_];
+
+        // 😱
+        if (cur.x == next.x
+            && next.x == obs.x
+            && ((cur.y < obs.y && obs.y <= next.y) || (cur.y > obs.y && obs.y >= next.y)))
+            || (cur.y == next.y
+                && next.y == obs.y
+                && ((cur.x < obs.x && obs.x <= next.x) || (cur.x > obs.x && obs.x >= next.x)))
+        {
+            next = obs - ORTHOGONAL[dir];
+        }
+
+        dir = turn_right(dir);
+        cur = next;
+    }
+    false
+}
+
+#[inline]
 fn find_tile(map: &Map<u8>, tile: u8) -> Pos { map.iter().find(|pos| map[pos] == tile).unwrap() }
+
+#[inline]
+fn key(p: Pos, dir: usize, map_size: Pos) -> usize { ((p.x * map_size.y + p.y) * 4) as usize + dir }
 
 #[inline]
 fn turn_right(dir: usize) -> usize { (dir + 1) % 4 }
